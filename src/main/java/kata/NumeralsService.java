60f9e9785f0e2188e3e76477f20263dc56ce1f1f
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2019-09-26 18:09:41 +0200

    1) Empty classes

---
+++ src/main/java/kata/NumeralsService.java
@@ -0,0 +1,8 @@
+package kata;
+
+public class NumeralsService {
+    public static int transform(String input) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/fr/axance/tech/kata/NumeralsTest.java
@@ -0,0 +1,12 @@
+package fr.axance.tech.kata;
+
+import kata.NumeralsService;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumeralsTest {
+
+
+
+}
