69605006267ea3fc847bab345ac0c83e7fd3f4f3
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2019-09-26 18:10:23 +0200

    2) First tests

--- src/main/java/kata/NumeralsService.java
+++ src/main/java/kata/NumeralsService.java
@@ -3,6 +3,18 @@ package kata;
 public class NumeralsService {
     public static int transform(String input) {
 
-        return 0;
+        if ("".equals(input) || input == null) {
+            return 0;
+        }
+
+        if (input.equals("I")) {
+            return 1;
+        }
+
+        if (input.equals("II")) {
+            return 2;
+        }
+
+        throw new NumberFormatException("Badly formatted roman numeral");
     }
 }
--- src/test/java/fr/axance/tech/kata/NumeralsTest.java
+++ src/test/java/fr/axance/tech/kata/NumeralsTest.java
@@ -7,6 +7,51 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumeralsTest {
 
+    @Test
+    public void must_print_1_when_I() {
+        //Given
+        String input = "I";
 
+        //When
+        int result = NumeralsService.transform(input);
+
+        //Then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void must_print_2_when_II() {
+        //Given
+        String input = "II";
+
+        //When
+        int result = NumeralsService.transform(input);
+
+        //Then
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void should_fail_when_input_is_not_formatted() {
+        //Given
+        String input = "IIa";
+
+        //When
+        int result = NumeralsService.transform(input);
+
+        //Then
+    }
+
+    @Test
+    public void should_return_0_when_input_empty() {
+        //Given
+        String input = "";
+
+        //When
+        int result = NumeralsService.transform(input);
+
+        //then
+        assertThat(result).isEqualTo(0);
+    }
 
 }
