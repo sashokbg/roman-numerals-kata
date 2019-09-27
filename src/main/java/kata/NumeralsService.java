688ad9950a01e19fab4f9131c493c7c2282ef871
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2019-09-27 13:31:31 +0200

    5) Reafctor for V

--- src/main/java/kata/NumeralsService.java
+++ src/main/java/kata/NumeralsService.java
@@ -9,15 +9,21 @@ public class NumeralsService {
             return 0;
         }
 
+        char previousCharacter = '\0';
+
         for (char character : input.toCharArray()) {
             if (character == 'I') {
                 result++;
             } else if (character == 'V') {
+                if (previousCharacter == 'I') {
+                    result-=2;
+                }
                 result+=5;
             }
             else {
                 throw new NumberFormatException("Badly formatted roman numeral");
             }
+            previousCharacter = character;
         }
 
         return result;
